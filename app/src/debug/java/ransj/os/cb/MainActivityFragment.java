976cf0cf62ccda08fb1861ed6221fa62171e2c0e
ransj <ranshengju@gmail.com>
2016-05-30 18:25:58 +0800

    init commit

---
+++ app/src/debug/java/ransj/os/cb/MainActivityFragment.java
@@ -0,0 +1,38 @@
+package ransj.os.cb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sample.moreteapots.MoreTeapotsNativeActivity;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent teapots = new Intent(getActivity(), MoreTeapotsNativeActivity.class);
+                startActivity(teapots);
+            }
+        });
+    }
+}
